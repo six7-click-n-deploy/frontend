cfd66396715d2b9667cc85ed47b8dfdbb2c715f6
root <root@Win-MonikaPjan.localdomain>
2026-01-08 21:13:57 +0100

    deleted comments

--- src/views/DeploymentsListView.vue
+++ src/views/DeploymentsListView.vue
@@ -5,8 +5,6 @@ import {
   BarChart3, 
   CircleArrowRight, 
   Plus, 
-  CheckCircle, // Neu: Für das Icon
-  X            // Neu: Zum Schließen
 } from 'lucide-vue-next'
 import BaseButton from '@/components/ui/BaseButton.vue'
 
@@ -39,16 +37,6 @@ watch(
 <template>
   <div class="p-6">
     
-   <!-- <div v-if="showSuccess" class="mb-8 p-4 bg-green-100 border border-green-300 text-green-800 rounded-lg flex items-center justify-between shadow-md">
-      <div class="flex items-center gap-3">
-        <CheckCircle :size="24" />
-        <span class="font-bold text-lg">201 Deployment erfolgreich</span>
-      </div>
-      <button @click="showSuccess = false">
-        <X :size="20" class="hover:text-green-900" />
-      </button>
-    </div>-->
-
     <div class="flex items-start justify-between mb-12">
       <div>
         <div class="flex items-center gap-6 text-primary mb-3">
