9b3ce7222d9b8780eec2c46101785c5fcc9e9506
root <root@Win-MonikaPjan.localdomain>
2026-01-23 19:47:38 +0100

    commit last changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,7 @@ export default {
         primaryDark: "#336A4A",
         primaryLight: "#4e7d67",
         lightGreen: "#b9d4c0ff",
-        ultraLightGreen: "rgb(219, 229, 222)" ,
+        ultraLightGreen: "#dbe5de" ,
 
         accentYellow: "#E48C2A",   // Gelb aus Logo
         lightYellow: "#fbe6cf",
